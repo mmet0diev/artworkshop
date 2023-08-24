1258c5b303f506d285793bf523d09823d4d46639
mmet0diev <max.metodiev.1999@gmail.com>
2023-08-24 16:06:24 +0300

    changed func names

--- js.js
+++ js.js
@@ -1,29 +1,29 @@
 // Define a function to change the language to English
-function switchToEnglish() {
+function switchToEnHome() {
     // Fetch and parse the English JSON data
-    fetch('langs/en.json')
+    fetch('langs/home_en.json')
         .then(response => response.json())
         .then(data => {
             // Update the content on your page with the English data
             document.getElementById('home-link').textContent = data.home;
             document.getElementById('gallery-link').textContent = data.gallery;
             document.getElementById('about-link').textContent = data.about;
-            document.getElementById('welcome-text').textContent = data.welcome;
-            document.getElementById('paragraph1').textContent = data.paragraph1;
+            document.getElementById('pic1').textContent = data.pic1;
+            document.getElementById('pic2').textContent = data.pic2;
         });
 }
 
 // Define a function to change the language to Bulgarian
-function switchToBulgarian() {
+function switchToBgHome() {
     // Fetch and parse the Bulgarian JSON data
-    fetch('langs/bg.json')
+    fetch('langs/home_bg.json')
         .then(response => response.json())
         .then(data => {
             // Update the content on your page with the Bulgarian data
             document.getElementById('home-link').textContent = data.home;
             document.getElementById('gallery-link').textContent = data.gallery;
             document.getElementById('about-link').textContent = data.about;
-            document.getElementById('welcome-text').textContent = data.welcome;
-            document.getElementById('paragraph1').textContent = data.paragraph1;
+            document.getElementById('pic1').textContent = data.pic1;
+            document.getElementById('pic2').textContent = data.pic2;
         });
 }
